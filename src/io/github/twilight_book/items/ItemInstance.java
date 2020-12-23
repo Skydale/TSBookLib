f11f580a6301b23e3b0a7352d49affd49693077e
raymondliu1 <unconfigured@null.spigotmc.org>
2020-12-23 12:17:43 +0800

    12/24/2020

--- src/io/github/twilight_book/items/ItemInstance.java
+++ src/io/github/twilight_book/items/ItemInstance.java
@@ -6,7 +6,9 @@ import org.bukkit.configuration.file.YamlConfiguration;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.plugin.java.JavaPlugin;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
 
 public class ItemInstance { //represents a single ItemStack
     private final String NAME;
@@ -15,6 +17,8 @@ public class ItemInstance { //represents a single ItemStack
     private final Material MATERIAL;
     private YamlConfiguration CONFIG;
     private int MODEL = 0;
+    private List<String> LoreWithStats = new ArrayList<>();
+    private ItemIdentification IDENTIFICATION;
 
     public ItemInstance(ConfigAbstract config, String ID, String path) {
         switch (path.toLowerCase()) {
@@ -69,16 +73,39 @@ public class ItemInstance { //represents a single ItemStack
         return LORE;
     }
 
+    public void updateLore(){
+        LoreWithStats.clear();
+        for(String s:LORE){
+            String l = s.replaceAll("\\[item.damage-physical]",IDENTIFICATION.getStats(ItemUtils.StatsType.PHYSICAL)+"")
+                    .replaceAll("\\[item.damage-terra]",IDENTIFICATION.getStats(ItemUtils.StatsType.TERRA)+"")
+                    .replaceAll("\\[item.damage-aer]",IDENTIFICATION.getStats(ItemUtils.StatsType.AER)+"")
+                    .replaceAll("\\[item.damage-ignis]",IDENTIFICATION.getStats(ItemUtils.StatsType.IGNIS)+"")
+                    .replaceAll("\\[item.damage-aqua]",IDENTIFICATION.getStats(ItemUtils.StatsType.AQUA)+"")
+                    .replaceAll("\\[item.damage-lumen]",IDENTIFICATION.getStats(ItemUtils.StatsType.LUMEN)+"")
+                    .replaceAll("\\[item.damage-umbra]",IDENTIFICATION.getStats(ItemUtils.StatsType.UMBRA)+"");
+            LoreWithStats.add(l);
+        }
+    }
+
+    public List<String> getLoreWithStats(){
+        return LoreWithStats;
+    }
+
     public YamlConfiguration getConfig(){
         return CONFIG;
     }
 
-    public int getModel() {
-        return MODEL;
+    @Nullable
+    public ItemIdentification getIdentification(){
+        return IDENTIFICATION;
     }
 
-    public Material getMaterial() {
-        return MATERIAL;
+    public void reIdentify(){
+        //TODO if(not identifiable) return;
+        if (IDENTIFICATION == null)
+            IDENTIFICATION = new ItemIdentification(CONFIG,new Random());
+        else
+            IDENTIFICATION.rollStats();
     }
 
     public ItemStack createItem(JavaPlugin plugin) {
